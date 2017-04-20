ca67b7d66a7781986ac0ff89013d3d88c34303c2
Michael Ruppe <michael.ruppe@live.com.au>
2017-04-20 11:47:46 +1000

    Merge regular gif and twitter functionality into a single file
    Now the tweeting behaviour is determined by the 'tweet' variable.
    The separate script for tweeting gifs has been removed.
    
    USB drive script is still untouched.

--- gifcam.py
+++ gifcam.py
@@ -5,11 +5,44 @@ import RPi.GPIO as GPIO
 from os import system
 import os
 import random, string
+from twython import Twython
 
+########################
+#
+# Behaviour Variables
+#
+########################
+num_frame = 8       # Number of frames in Gif
+gif_delay = 15      # Frame delay [ms]
+rebound = True      # Create a video that loops start <=> end
+tweet = False       # Tweets the GIF after capturing
+
+
+########################
+#
+# Twitter (Optional)
+# Ensure 'tweet' behaviour-variable is True if you want to tweet pictures.
+#
+########################
+APP_KEY = 'YOUR API KEY'
+APP_SECRET = 'YOUR API SECRET'
+OAUTH_TOKEN = 'YOUR ACCESS TOKEN'
+OAUTH_TOKEN_SECRET = 'YOUR ACCESS TOKEN SECRET'
+
+#setup the twitter api client
+twitter = Twython(APP_KEY, APP_SECRET,
+                  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+
+########################
+#
+# Define GPIO
+#
+########################
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-button = 19 #Button GPIO Pin
 
+button = 19 #Button GPIO Pin
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 led_1 = 12 #Status LED GPIO Pin
 GPIO.setup(led_1, GPIO.OUT)
@@ -18,13 +51,12 @@ led_2 = 21 #ON/OFF LED Pin
 GPIO.setup(led_2, GPIO.OUT)
 statusLed = GPIO.PWM(led_2, 2)
 
-########################
-### Variables Config ###
-########################
-num_pics = 8        # Number of pictures to take in Gif
-gif_delay = 15      # How much delay in between those pictures (in milliseconds)
-rebound = True      # create a video that loops start <=> end
 
+########################
+#
+# Camera
+#
+########################
 camera = picamera.PiCamera()
 camera.resolution = (540, 405)
 camera.rotation = 90
@@ -41,29 +73,40 @@ print('System Ready')
 def random_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
+def tweet_pics():
+    try:
+        print('Posting to Twitter')
+        photo = open(filename + ".gif", 'rb')
+        response = twitter.upload_media(media=photo)
+        twitter.update_status(status='Taken with LoFi Gif Camera', media_ids=[response['media_id']])
+    except:
+        # Display error with long status light
+        statusLed.ChangeDutyCycle(100)
+        buttonLed.ChangeDutyCycle(0)
+        sleep(2)
+        
+
 try:
     while True:
-        input_state = GPIO.input(button) # Sense the button
+        if GPIO.input(button) == False: # Button Pressed
         
-        if input_state == False:
-
             ### TAKING PICTURES ###
             print('Gif Started')
             statusLed.ChangeDutyCycle(0)
             buttonLed.ChangeDutyCycle(50)
 
             randomstring = random_generator()
-            for i in range(num_pics):
+            for i in range(num_frame):
                 camera.capture('{0:04d}.jpg'.format(i))
 
-            ### PROCESSING PICTURES ###
+            ### PROCESSING GIF ###
             statusLed.ChangeDutyCycle(50)
             buttonLed.ChangeDutyCycle(0)
             if rebound == True: # make copy of images in reverse order
-                for i in range(num_pics - 1):
-                    source = str(num_pics - i - 1) + ".jpg"
+                for i in range(num_frame - 1):
+                    source = str(num_frame - i - 1) + ".jpg"
                     source = source.zfill(8) # pad with zeros
-                    dest = str(num_pics + i) + ".jpg"
+                    dest = str(num_frame + i) + ".jpg"
                     dest = dest.zfill(8) # pad with zeros
                     copyCommand = "cp " + source + " " + dest
                     os.system(copyCommand)
@@ -73,14 +116,21 @@ try:
             graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + "*.jpg " + filename + ".gif" 
             os.system(graphicsmagick)
             os.system("rm ./*.jpg") # cleanup source images
+
+            ### TWEETING ###
+            if tweet == True:
+                statusLed.ChangeDutyCycle(25)
+                buttonLed.ChangeDutyCycle(0)
+                tweet_pics()
+            
             print('Done')
             print('System Ready')
 
-        else :
-            ### READY TO TAKE PICTURES ###
+        else : # Button NOT pressed
+            ### READY TO MAKE GIF ###
             statusLed.ChangeDutyCycle(0)
             buttonLed.ChangeDutyCycle(100)
-            
+            sleep(0.05)
            
 except:
     GPIO.cleanup()
